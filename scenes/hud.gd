fa5b36e5d1df5be3f4a4823ca4ce9f3e32c16686
sergiusz <gusevxsergey@gmail.com>
2024-01-30 23:54:50 +0100

    added HUD with game score

---
+++ scenes/hud.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+@onready var score_label = $MarginContainer/score_label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	score_label.text = str(ScoreManager.get_score())
