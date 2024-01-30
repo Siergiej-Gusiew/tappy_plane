b0766fa2c3f6494c618523d574d5221033fccad0
sergiusz <gusevxsergey@gmail.com>
2024-01-30 19:47:13 +0100

    now if the game is over - the pipes are not moving

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -7,6 +7,7 @@ var score: int = 0
 @export var pipes_scene: PackedScene
 
 @onready var pipes_holder = $pipes_holder
+@onready var spawn_timer = $spawn_timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -25,9 +26,17 @@ func spawn_pipes() -> void:
 	new_pipes.position = Vector2(1250, pos_y)
 	pipes_holder.add_child(new_pipes)
 
+
+func stop_pipes() -> void:
+	spawn_timer.stop()
+	for pipe in pipes_holder.get_children():
+		pipe.set_process(false)
+
+
 func _on_spawn_timer_timeout():
 	spawn_pipes()
 
 
 func _on_plane_crushed():
-	print("the plane is crushed")
+	stop_pipes()
+	# TODO: paste here plane crush animation or scene!
