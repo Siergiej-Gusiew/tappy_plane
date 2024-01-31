958e776d32bb4dfc49a762e451a36fa590a6ada1
sergiusz <gusevxsergey@gmail.com>
2024-01-31 19:59:45 +0100

    added 'game over' scene

---
+++ scenes/game_over/game_over.gd
@@ -0,0 +1,33 @@
+extends Control
+
+
+@onready var game_over_label = $GameOverLabel
+@onready var space_label = $SpaceLabel
+@onready var timer = $Timer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	hide()
+	space_label.hide()
+	SignalManager.on_plane_crushed.connect(_on_plane_crushed)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if space_label.visible and Input.is_action_just_pressed("fly"):
+		GameManager.load_main_scene()
+	
+	
+func run_sequence() -> void:
+	game_over_label.hide()
+	space_label.show()
+
+
+func _on_plane_crushed() -> void:
+	show()
+	timer.start()
+
+
+func _on_timer_timeout():
+	run_sequence()
